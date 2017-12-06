fcacea6b5255f30cf0943966e553c73aa69b8ecf
gingleton <georgeingleton@gmail.com>
2017-12-05 22:55:49 -0500

    react TKH HW

--- app/index.js
+++ app/index.js
@@ -1,16 +1,42 @@
-var React = require('react');
+var React = require('react'),
 var ReactDOM = require('react-dom');
-require('./index.css');
 
-class App extends React.Component {
+class Users extends React.Component {
   render() {
     return (
-      <div>Hello World!</div>
+      <div>
+        <h1>Friends</h1>
+        <ul>
+          {this.props.list.filter(function (user)  {
+          return user.friend === true
+          }).map(function (user) {
+          return <li>{user.name}</li>
+          })}
+        </ul>
+
+        <hr />
+
+        <h1> Non Friends </h1>
+        <ul>
+          {this.props.list.filter(function(user) {
+            return user.friend === false;
+          }).map(function (user) {
+            return <li>{user.name}</li>
+          })}
+        </ul>
+      </div>
     )
   }
 }
 
 ReactDOM.render(
-  <App />,
+  <Users list={[
+    { name: 'Tyler', friend: true },
+    { name: 'Ryan', friend: true },
+    { name: 'Michael', friend: false },
+    { name: 'Mikenzi', friend: false },
+    { name: 'Jessica', friend: true },
+    { name: 'Dan', friend: false } ]}
+  />,
   document.getElementById('app')
 );
