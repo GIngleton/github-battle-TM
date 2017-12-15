6bd20766abb642cb53dc91fa2ca2848c58f39225
gingleton <georgeingleton@gmail.com>
2017-12-14 20:12:39 -0500

    TM React fundameltals through section 10

--- app/index.js
+++ app/index.js
@@ -1,42 +1,9 @@
-var React = require('react'),
+var React = require('react');
 var ReactDOM = require('react-dom');
-
-class Users extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Friends</h1>
-        <ul>
-          {this.props.list.filter(function (user)  {
-          return user.friend === true
-          }).map(function (user) {
-          return <li>{user.name}</li>
-          })}
-        </ul>
-
-        <hr />
-
-        <h1> Non Friends </h1>
-        <ul>
-          {this.props.list.filter(function(user) {
-            return user.friend === false;
-          }).map(function (user) {
-            return <li>{user.name}</li>
-          })}
-        </ul>
-      </div>
-    )
-  }
-}
+require('./index.css');
+var App = require('./components/App');
 
 ReactDOM.render(
-  <Users list={[
-    { name: 'Tyler', friend: true },
-    { name: 'Ryan', friend: true },
-    { name: 'Michael', friend: false },
-    { name: 'Mikenzi', friend: false },
-    { name: 'Jessica', friend: true },
-    { name: 'Dan', friend: false } ]}
-  />,
-  document.getElementById('app')
-);
+	<App />,
+	document.getElementById('app')
+	)
